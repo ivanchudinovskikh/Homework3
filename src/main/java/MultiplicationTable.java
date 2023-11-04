3ffd75bb189efea98b5bf2359f6754375d06edb8
Ivan <johntsuchi@gmail.com>
2023-11-04 22:22:15 +0400

    Initial Commit

--- src/main/java/MultiplicationTable.java
+++ src/main/java/MultiplicationTable.java
@@ -2,7 +2,7 @@ public class MultiplicationTable {
     public static void main(String[] args) {
         for (int i = 1; i <= 10; i++) {
             for (int k = 1; k <= 10; k++) {
-                System.out.print(k * i + " ");
+                System.out.print(k * i + "\t");
             }
             System.out.println(" ");
         }
