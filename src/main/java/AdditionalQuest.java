b34364c5e5863945666052b924688c753d24f120
Ivan <johntsuchi@gmail.com>
2023-11-04 20:41:57 +0400

    Initial Commit

--- src/main/java/AdditionalQuest.java
+++ src/main/java/AdditionalQuest.java
@@ -3,7 +3,7 @@ public class AdditionalQuest {
         int time = 0;
         int price = 0;
         int temp;
-        for (int i = 100; i >= 0; i = i-6, time++) {
+        for (int i = 100; i >= 0; i = i - 6, time++) {
             temp = i * 100;
             price = price + temp;
         }
